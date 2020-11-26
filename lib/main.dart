84dafad75780c76b6c32d0b8faf71b7ceb52accb
eyuzgul <eyuzgul@gmail.com>
2020-11-26 08:31:33 +0300

    adsf

--- lib/main.dart
+++ lib/main.dart
@@ -1,153 +1,82 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class Post {
+Future<Album> fetchAlbum() async {
+  final response =
+      await http.get('https://jsonplaceholder.typicode.com/albums/79');
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Hata oluştu222');
+  }
+}
+
+class Album {
   final int userId;
   final int id;
   final String title;
-  final String body;
 
-  Post(this.userId, this.id, this.title, this.body);
+  Album({this.userId, this.id, this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(userId: json['userId'], id: json['id'], title: json['title']);
+  }
 }
 
-void main() {
-  runApp(ListViewApp());
-}
+void main() => runApp(MyApp());
 
-class ListViewApp extends StatefulWidget {
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
   @override
-  _ListViewState createState() => new _ListViewState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _ListViewState extends State<ListViewApp> {
-  final List<Post> items = new List();
+class _MyAppState extends State<MyApp> {
+  Future<Album> futureAlbum;
 
   @override
   void initState() {
     super.initState();
-    setState(() {
-      items.add(new Post(
-        1,
-        2,
-        'qui est esse',
-        'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
-      ));
-      items.add(new Post(
-        1,
-        3,
-        'ea molestias quasi exercitationem repellat qui ipsa sit aut',
-        'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut',
-      ));
-      items.add(new Post(
-        2,
-        4,
-        'eum et est occaecati',
-        'ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit',
-      ));
-      items.add(new Post(
-        2,
-        5,
-        'nesciunt quas odio',
-        'repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque',
-      ));
-      items.add(new Post(
-        3,
-        6,
-        'dolorem eum magni eos aperiam quia',
-        'ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae',
-      ));
-      items.add(new Post(
-        3,
-        7,
-        'magnam facilis autem',
-        'dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas',
-      ));
-      items.add(new Post(
-        3,
-        8,
-        'dolorem dolore est ipsam',
-        'dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae',
-      ));
-    });
+    futureAlbum = fetchAlbum();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'JSA ListView Demo',
+      title: 'Http ornegi',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('ListView Demo'),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
+          title: Text('Http denemesi'),
         ),
         body: Center(
-          child: ListView.builder(
-              itemCount: items.length,
-              padding: const EdgeInsets.all(15.0),
-              itemBuilder: (context, position) {
-                return Dismissible(
-                    key: Key(items[position].id.toString()),
-                    onDismissed: (direction) {
-                      setState(() {
-                        items.removeAt(position);
-                      });
-                    },
-                    background: Container(
-                      color: Colors.redAccent,
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Divider(height: 5.0),
-                        ListTile(
-                          title: Text(
-                            '${items[position].title}',
-                            style: TextStyle(
-                              fontSize: 22.0,
-                              color: Colors.deepOrangeAccent,
-                            ),
-                          ),
-                          subtitle: Text(
-                            '${items[position].body}',
-                            style: new TextStyle(
-                              fontSize: 18.0,
-                              fontStyle: FontStyle.italic,
-                            ),
-                          ),
-                          leading: Column(
-                            children: <Widget>[
-                              CircleAvatar(
-                                backgroundColor: Colors.blueAccent,
-                                radius: 35.0,
-                                child: Text(
-                                  'User ${items[position].userId}',
-                                  style: TextStyle(
-                                    fontSize: 22.0,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                              ),
-                              IconButton(
-                                icon: const Icon(Icons.remove_circle_outline),
-                                onPressed: () {
-                                  setState(() {
-                                    items.removeAt(position);
-                                  });
-                                },
-                              ),
-                            ],
-                          ),
-                          onTap: () => _onTapItem(context, items[position]),
-                        ),
-                      ],
-                    ));
+          child: FutureBuilder<Album>(
+              future: futureAlbum,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(
+                    children: [
+                      Text(
+                          "user: " +
+                              snapshot.data.userId.toString() +
+                              " - id: " +
+                              snapshot.data.id.toString(),
+                          style: TextStyle(fontSize: 30, color: Colors.red)),
+                      Text(snapshot.data.title,
+                          style: TextStyle(fontSize: 30, color: Colors.red)),
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+
+                return CircularProgressIndicator();
               }),
         ),
       ),
     );
-  }
-
-  void _onTapItem(BuildContext context, Post post) {
-    Scaffold.of(context).showSnackBar(new SnackBar(
-        content: new Text(post.id.toString() + ' - ' + post.title)));
   }
 }
